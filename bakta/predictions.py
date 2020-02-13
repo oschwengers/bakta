afc2788e9e8551970c806a5a7ef9b922a978f311
Oliver Schwengers <oliver@schwengers.de>
2020-02-13 14:08:38 +0100

    annotate tRNAs with specific SO terms

--- bakta/predictions.py
+++ bakta/predictions.py
@@ -56,6 +56,30 @@ def predict_t_rnas(data, contigs_path):
 
             contig = contig.strip()  # bugfix for extra single whitespace in tRNAscan-SE output
 
+            so_terms = {
+                'ala': 'SO:0000254',  # alanyl_tRNA
+                'gln': 'SO:0000261',  # glycyl_tRNA
+                'glu': 'SO:0000259',  # glutaminyl_tRNA
+                'gly': 'SO:0000260',  # glutamyl_tRNA
+                'pro': 'SO:0000268',  # prolyl_tRNA
+                'met': 'SO:0000266',  # methionyl_tRNA
+                'asp': 'SO:0000256',  # asparaginyl_tRNA
+                'thr': 'SO:0000270',  # threonyl_tRNA
+                'val': 'SO:0000273',  # valyl_tRNA
+                'tyr': 'SO:0000272',  # tyrosyl_tRNA
+                'cys': 'SO:0000258',  # cysteinyl_tRNA
+                'ile': 'SO:0000263',  # isoleucyl_tRNA
+                'ser': 'SO:0000269',  # seryl_tRNA
+                'leu': 'SO:0000264',  # leucyl_tRNA
+                'trp': 'SO:0000271',  # tryptophanyl_tRNA
+                'lys': 'SO:0000265',  # lysyl_tRNA
+                'asn': 'SO:0000257',  # aspartyl_tRNA
+                'arg': 'SO:0001036',  # arginyl_tRNA
+                'his': 'SO:0000262',  # histidyl_tRNA
+                'phe': 'SO:0000267',  # phenylalanyl_tRNA
+                'selcys': 'SO:0005857'  # selenocysteinyl_tRNA
+            }
+
             trna = {
                 'type': bc.INSDC_FEATURE_T_RNA,
                 'gene': "%s_trna" % type,
@@ -68,8 +92,9 @@ def predict_t_rnas(data, contigs_path):
                 'score': float(score),
                 'pseudo': 'pseudo' in note,
                 'notes': ["tRNA-%s(%s)" % (type, anti_codon)],
-                'db_xrefs': ['SO:0001272']
+                'db_xrefs': so_terms.get(type.lower(), [])
             }
+
             key = "%s.trna%s" % (contig, trna_id)
             log.debug("key=%s", key)
             trnas[key] = trna
@@ -146,7 +171,7 @@ def predict_tm_rnas(data, contigs_path):
                     'strand': strand,
                     'sequence': seq,
                     'inference': 'aragorn',
-                    'db_xrefs': ['SO:0001272']
+                    'db_xrefs': ['SO:0000584']
                 }
                 tmrnas.append(tmrna)
                 log.info(
