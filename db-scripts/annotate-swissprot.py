a6969787e937049095c0d2a2d189642b4f79c30f
Oliver Schwengers <oliver@schwengers.de>
2020-08-31 22:57:17 +0200

    add swissprot db script #12

---
+++ db-scripts/annotate-swissprot.py
@@ -0,0 +1,148 @@
+import argparse
+import hashlib
+import gzip
+import sqlite3
+from pathlib import Path
+import xml.etree.ElementTree as et
+
+
+parser = argparse.ArgumentParser(
+    description='Filter Uniprot\'s UniRef100 XML files to bacterial subsequences and create initial ups db.'
+)
+parser.add_argument('--taxonomy', action='store', help='Path to NCBI taxonomy node.dmp file.')
+parser.add_argument('--xml', action='store', help='Path to SwissProt xml file.')
+parser.add_argument('--db', action='store', help='Path to Bakta sqlite3 db file.')
+args = parser.parse_args()
+
+taxonomy_path = Path(args.taxonomy).resolve()
+xml_path = Path(args.xml).resolve()
+db_path = Path(args.db)
+
+
+def is_taxon_child(child, LCA, taxonomy):
+    parent = taxonomy.get(child, None)
+    while(parent is not None and parent != '1'):
+        if(parent == LCA):
+            return True
+        else:
+            parent = taxonomy.get(parent, None)
+    return False
+
+
+print('parse & store NCBI taxonomy...')
+taxonomy = {}
+with taxonomy_path.open() as fh:
+    for line in fh:
+        cols = line.split('\t|\t', maxsplit=2)
+        taxonomy[cols[0]] = cols[1]
+print("\tstored tax ids: %d" % len(taxonomy))
+
+sp_processed = 0
+sp_not_found = 0
+sp_lenth_mm = 0
+
+ups_annotated = 0
+psc_annotated = 0
+
+print('parse & store SwissProt information...')
+ns = 'http://uniprot.org/uniprot'
+with sqlite3.connect(str(db_path), isolation_level='EXCLUSIVE') as conn:
+    conn.execute('PRAGMA page_size = 4096;')
+    conn.execute('PRAGMA cache_size = 100000;')
+    conn.execute('PRAGMA locking_mode = EXCLUSIVE;')
+    conn.execute("PRAGMA mmap_size = %i;" % (20 * 1024 * 1024 * 1024))
+    conn.execute('PRAGMA synchronous = OFF;')
+    conn.execute('PRAGMA journal_mode = OFF')
+    conn.execute('PRAGMA threads = 2;')
+    conn.commit()
+
+    conn.row_factory = sqlite3.Row
+    c = conn.cursor()
+    with gzip.open(str(xml_path), mode="rt") as fh:
+        ups_entries = []
+        i = 0
+        for event, elem in et.iterparse(fh):
+            # print(elem)
+            if(elem.tag == "{%s}entry" % ns):
+                # print(elem)
+                sp_processed += 1
+                tax_property = elem.find("./{%s}organism/{%s}dbReference[@type='NCBI Taxonomy']" % (ns, ns))
+                if(tax_property is not None):
+                    tax_id = tax_property.get('id')
+                    if is_taxon_child(tax_id, '2', taxonomy):
+                        acc = elem.find("./{%s}accession" % ns).text
+                        gene = elem.find("./{%s}gene/{%s}name[@type='primary']" % (ns, ns))
+                        gene = '' if gene is None else gene.text
+                        product = elem.find("./{%s}protein/{%s}recommendedName/{%s}fullName" % (ns, ns, ns))
+                        product = '' if product is None else product.text
+                        ec_id = elem.find("./{%s}protein/{%s}recommendedName/{%s}ecNumber" % (ns, ns, ns))
+                        ec_id = '' if ec_id is None else ec_id.text
+                        seq = elem.find("./{%s}sequence" % ns).text.upper()
+                        hash = hashlib.md5(seq.encode()).hexdigest()
+                        go_terms = []
+                        for go_term in elem.findall("./{%s}dbReference[@type='GO']" % ns):
+                            go_terms.append(go_term.get('id'))
+                        try:
+                            c.execute('SELECT * FROM ups WHERE hash=?', (hash,))
+                            r = c.fetchone()
+                            if(r is None):
+                                print("WARNING: could not find UPS! swissprot-acc=%s, hash=%s" % (acc, hash))
+                                sp_not_found += 1
+                                continue
+                            elif(r['length'] != len(seq)):
+                                print("WARNING: length mismatch! swissprot-acc=%s, hash=%s, ups-length=%d, swissprot-length=%d" % (acc, hash, r['length'], len(seq)))
+                                sp_lenth_mm += 1
+                                continue
+                            else:
+                                # print("parse SP: acc=%s, gene=%s, product=%s, ec=%s" % (acc, gene, product, ec_id))
+                                # print("found ups: uniref100=%s, uniref90=%s, gene=%s, product=%s, ec=%s" % (r['uniref100_id'], r['uniref90_id'], r['gene'], r['product'], r['ec_id']))
+                                c.execute('SELECT * FROM psc WHERE uniref90_id=?', (r['uniref90_id'],))
+                                r = c.fetchone()
+                                if(r is None):  # no PSC found -> annotate UPS
+                                    # print("no PSC found! annotate UPS")
+                                    update = False
+                                    if(gene):
+                                        # print("UPDATE ups SET gene=%s WHERE hash=%s" % (gene, hash))
+                                        c.execute('UPDATE ups SET gene=? WHERE hash=?', (gene, hash))
+                                        update = True
+                                    if(product):
+                                        # print("UPDATE ups SET product=%s WHERE hash=%s" % (product, hash))
+                                        c.execute('UPDATE ups SET product=? WHERE hash=?', (product, hash))
+                                        update = True
+                                    if(ec_id):
+                                        # print("UPDATE ups SET ec_id=%s WHERE hash=%s" % (ec_id, hash))
+                                        c.execute('UPDATE ups SET ec_id=? WHERE hash=?', (ec_id, hash))
+                                        update = True
+                                    if(update):
+                                        ups_annotated += 1
+                                else:  # PSC found -> annotate PSC
+                                    # print("PSC found! annotate PSC")
+                                    update = False
+                                    if(gene):
+                                        # print("UPDATE psc SET gene=%s WHERE uniref90_id=%s" % (gene, r['uniref90_id']))
+                                        c.execute('UPDATE psc SET gene=? WHERE uniref90_id=?', (gene, r['uniref90_id']))
+                                        update = True
+                                    if(product):
+                                        # print("UPDATE psc SET product=%s WHERE uniref90_id=%s" % (product, r['uniref90_id']))
+                                        c.execute('UPDATE psc SET product=? WHERE uniref90_id=?', (product, r['uniref90_id']))
+                                        update = True
+                                    if(ec_id):
+                                        # print("UPDATE psc SET ec_id=%s WHERE uniref90_id=%s" % (ec_id, r['uniref90_id']))
+                                        c.execute('UPDATE psc SET ec_id=? WHERE uniref90_id=?', (ec_id, r['uniref90_id']))
+                                        update = True
+                                    if(update):
+                                        psc_annotated += 1
+                            if((sp_processed % 10000) == 0):
+                                conn.commit()
+                                print("\t... %d" % sp_processed)
+                        except sqlite3.Error as e:
+                            print("SQL ERROR: could not set annotation for sw-id=%s" % acc)
+                            print(e)
+                elem.clear()  # forstall out of memory errors
+
+print('\n')
+print("SwissProt proteins not found: %d" % sp_not_found)
+print("Hash protein length collision: %d" % sp_lenth_mm)
+
+print("UPSs annotated: %d" % ups_annotated)
+print("PSCs annotated: %d" % psc_annotated)
--- db-scripts/buid-db.sh
+++ db-scripts/buid-db.sh
@@ -8,7 +8,7 @@ cd db
 printf "Create Bakta database\n"
 
 # download rRNA covariance models from Rfam
-printf "\n1/10: download rRNA covariance models from Rfam ...\n"
+printf "\n1/11: download rRNA covariance models from Rfam ...\n"
 mkdir Rfam
 cd Rfam
 wget ftp://ftp.ebi.ac.uk/pub/databases/Rfam/CURRENT/Rfam.tar.gz
@@ -22,7 +22,7 @@ rm -r Rfam rRNA
 
 
 # download and extract ncRNA gene covariance models from Rfam
-printf "\n2/10: download ncRNA gene covariance models from Rfam ...\n"
+printf "\n2/11: download ncRNA gene covariance models from Rfam ...\n"
 mysql --user rfamro --host mysql-rfam-public.ebi.ac.uk --port 4497 --database Rfam < ${BAKTA_DB_SCRIPTS}/ncRNA-genes.sql > rfam-genes.raw.txt
 tail -n +2 rfam-genes.raw.txt > rfam-genes.txt
 wget ftp://ftp.ebi.ac.uk/pub/databases/Rfam/CURRENT/Rfam.cm.gz
@@ -35,7 +35,7 @@ rm rfam-genes.raw.txt rfam-genes.txt Rfam.cm.gz rfam2go ncRNA-genes
 
 
 # download and extract ncRNA regions (cis reg elements) covariance models from Rfam
-printf "\n2/10: download ncRNA region covariance models from Rfam ...\n"
+printf "\n2/11: download ncRNA region covariance models from Rfam ...\n"
 mysql --user rfamro --host mysql-rfam-public.ebi.ac.uk --port 4497 --database Rfam < ${BAKTA_DB_SCRIPTS}/ncRNA-regions.sql > rfam-regions.raw.txt
 tail -n +2 rfam-regions.raw.txt > rfam-regions.txt
 cmfetch -o ncRNA-regions -f Rfam.cm rfam-regions.txt
@@ -44,7 +44,7 @@ rm rfam-regions.raw.txt rfam-regions.txt Rfam.cm ncRNA-regions
 
 
 # download NCBI Taxonomy DB
-printf "\n3/10: download NCBI Taxonomy DB ...\n"
+printf "\n3/11: download NCBI Taxonomy DB ...\n"
 mkdir taxonomy
 cd taxonomy
 wget ftp://ftp.ncbi.nlm.nih.gov/pub/taxonomy/taxdump.tar.gz
@@ -57,7 +57,7 @@ rm -rf taxonomy
 ############################################################################
 # Setup SQLite Bakta db
 ############################################################################
-printf "\n4/10: setup SQLite Bakta db ...\n"
+printf "\n4/11: setup SQLite Bakta db ...\n"
 python3 ${BAKTA_DB_SCRIPTS}/init-db.py --db bakta.db
 
 
@@ -66,9 +66,9 @@ python3 ${BAKTA_DB_SCRIPTS}/init-db.py --db bakta.db
 # - download UniProt UniRef100
 # - read, filter and transform UniRef100 entries and store to ups.db
 ############################################################################
-printf "\n5/10: download UniProt UniRef100 ...\n"
+printf "\n5/11: download UniProt UniRef100 ...\n"
 wget -nv ftp://ftp.expasy.org/databases/uniprot/current_release/uniref/uniref100/uniref100.xml.gz
-printf "\n5/10: read, filter and store UniRef100 entries ...:\n"
+printf "\n5/11: read, filter and store UniRef100 entries ...:\n"
 python3 ${BAKTA_DB_SCRIPTS}/init-ups.py --taxonomy nodes.dmp --xml uniref100.xml.gz --db bakta.db
 rm uniref100.xml.gz
 
@@ -79,11 +79,11 @@ rm uniref100.xml.gz
 # - read and transform UniRef90 XML file to DB and Fasta file
 # - build PSC Diamond db
 ############################################################################
-printf "\n6/10: download UniProt UniRef90 ...\n"
+printf "\n6/11: download UniProt UniRef90 ...\n"
 wget -nv ftp://ftp.expasy.org/databases/uniprot/current_release/uniref/uniref90/uniref90.xml.gz
-printf "\n6/10: read UniRef90 entries and build Protein Sequence Cluster sequence and information databases:\n"
+printf "\n6/11: read UniRef90 entries and build Protein Sequence Cluster sequence and information databases:\n"
 python3 ${BAKTA_DB_SCRIPTS}/init-psc.py --taxonomy nodes.dmp --xml uniref90.xml.gz --db bakta.db --fasta psc.faa
-printf "\n5/10: build PSC Diamond db ...\n"
+printf "\n5/11: build PSC Diamond db ...\n"
 diamond makedb --in psc.faa --db psc
 rm uniref90.xml.gz
 
@@ -94,7 +94,7 @@ rm uniref90.xml.gz
 # - annotate UPSs with NCBI nrp IDs (WP_*)
 # - annotate PSCs with NCBI gene names (WP_* -> hash -> UniRef100 -> UniRef90 -> PSC)
 ############################################################################
-printf "\n7/10: download RefSeq nonredundant proteins and clusters ...\n"
+printf "\n7/11: download RefSeq nonredundant proteins and clusters ...\n"
 wget -nv ftp://ftp.ncbi.nlm.nih.gov/genomes/CLUSTERS/PCLA_proteins.txt
 wget -nv ftp://ftp.ncbi.nlm.nih.gov/genomes/CLUSTERS/PCLA_clusters.txt
 for i in {1..1091}; do
@@ -102,9 +102,22 @@ for i in {1..1091}; do
     pigz -dc bacteria.nonredundant_protein.${i}.protein.faa.gz | seqtk seq -CU >> refseq-bacteria-nrp.trimmed.faa
     rm bacteria.nonredundant_protein.${i}.protein.faa.gz
 done
-printf "\n7/10: annotate UPSs and PSCs ...\n"
+printf "\n7/11: annotate UPSs and PSCs ...\n"
 python3 ${BAKTA_DB_SCRIPTS}/annotate-ncbi-nrp.py --db bakta.db --nrp refseq-bacteria-nrp.trimmed.faa --pcla-proteins PCLA_proteins.txt --pcla-clusters PCLA_clusters.txt
 rm refseq-bacteria-nrp.trimmed.faa PCLA_proteins.txt PCLA_clusters.txt
+
+
+############################################################################
+# Integrate UniProt Swissprot information
+# - download SwissProt annotation xml file
+# - annotate PSCs if UPS have PSC UniRef90 identifier (seq -> hash -> UniRef100 -> UniRef90 -> PSC)
+# - annotate UPSs if UPS have no PSC UniRef90 identifier (seq -> hash -> UniRef100)
+############################################################################
+# printf "\n8/11: download UniProt/SwissProt ...\n"
+wget ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_sprot.xml.gz
+printf "\n8/11: annotate UPSs and PSCs ...\n"
+python3 ${BAKTA_DB_SCRIPTS}/annotate-swissprot.py --taxonomy nodes.dmp --xml uniprot_sprot.xml.gz --db bakta.db
+rm uniprot_sprot.xml.gz
 
 
 ############################################################################
@@ -113,12 +126,12 @@ rm refseq-bacteria-nrp.trimmed.faa PCLA_proteins.txt PCLA_clusters.txt
 # - align UniRef90 proteins to COG protein sequences
 # - annotate PSCs with COG info
 ############################################################################
-printf "\n8/10: download COG db ...\n"
+printf "\n9/11: download COG db ...\n"
 wget -nv ftp://ftp.ncbi.nih.gov/pub/COG/COG2014/data/cognames2003-2014.tab  # COG IDs and functional class
 wget -nv ftp://ftp.ncbi.nih.gov/pub/COG/COG2014/data/cog2003-2014.csv # Mapping GenBank IDs -> COG IDs
 wget -nv ftp://ftp.ncbi.nih.gov/pub/COG/COG2014/data/prot2003-2014.fa.gz  # annotated protein sequences
 pigz -dc prot2003-2014.fa.gz | seqtk seq -CU > cog.faa
-printf "\n8/10: annotate PSCs ...\n"
+printf "\n9/11: annotate PSCs ...\n"
 diamond makedb --in cog.faa --db cog.dmnd
 diamond blastp --query psc.faa --db cog.dmnd --id 90 --query-cover 80 --subject-cover 80 --max-target-seqs 1 --out diamond.tsv --outfmt 6 qseqid sseqid length pident qlen slen evalue
 python3 ${BAKTA_DB_SCRIPTS}/annotate-cog.py --db bakta.db --alignments diamond.tsv --cog-ids cognames2003-2014.tab --gi-cog-mapping cog2003-2014.csv
@@ -130,9 +143,9 @@ rm cognames2003-2014.tab cog2003-2014.csv prot2003-2014.fa.gz diamond.tsv cog.*
 # - download AMR gene WP_* annotations from NCBI Pathogen AMR db
 # - annotate UPSs with AMR info
 ############################################################################
-printf "\n9/10: download AMR gene WP_* annotations from NCBI Pathogen AMR db ...\n"
+printf "\n10/11: download AMR gene WP_* annotations from NCBI Pathogen AMR db ...\n"
 wget -nv ftp://ftp.ncbi.nlm.nih.gov/pathogen/Antimicrobial_resistance/Data/latest/ReferenceGeneCatalog.txt
-printf "\n9/10: annotate UPSs ...\n"
+printf "\n10/11: annotate UPSs ...\n"
 python3 ${BAKTA_DB_SCRIPTS}/annotate-ncbi-amr.py --db bakta.db --amr ReferenceGeneCatalog.txt
 rm ReferenceGeneCatalog.txt
 
@@ -142,9 +155,9 @@ rm ReferenceGeneCatalog.txt
 # - download IS protein sequences from GitHub (thanhleviet/ISfinder-sequences)
 # - annotate UPSs with IS info
 ############################################################################
-printf "\n10/10: download ISfinder protein sequences ...\n"
+printf "\n11/11: download ISfinder protein sequences ...\n"
 wget -nv https://raw.githubusercontent.com/thanhleviet/ISfinder-sequences/master/IS.faa
-printf "\n10/10: annotate UPSs ...\n"
+printf "\n11/11: annotate UPSs ...\n"
 diamond makedb --in IS.faa --db is
 diamond blastp --query psc.faa --db is.dmnd --id 98 --query-cover 99 --subject-cover 99 --max-target-seqs 1 --out diamond.tsv --outfmt 6 qseqid sseqid stitle length pident qlen slen evalue
 python3 ${BAKTA_DB_SCRIPTS}/annotate-is.py --db bakta.db --alignments diamond.tsv
