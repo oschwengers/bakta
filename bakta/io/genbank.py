b5aa5db12f0737f42bdb74b7605431c697e3d8aa
Oliver Schwengers <oliver@schwengers.de>
2020-11-17 08:27:04 +0100

    fix & review genbank export

--- bakta/io/genbank.py
+++ bakta/io/genbank.py
@@ -49,94 +49,88 @@ def write_genbank(genome, features, genbank_path):
 
         for feature in features:
             if(feature['contig'] == contig['id']):
-                feature_type = feature['type']
                 insdc_feature_type = None
                 qualifiers = {}
-                if(feature_type == bc.FEATURE_ORIC) or (feature_type == bc.FEATURE_ORIT) or (feature_type == bc.FEATURE_ORIV):
-                    #TODO: Add fuzzy positions for oriCTV
-                    qualifiers['strand'] = feature['strand']
-                    if(feature_type == bc.FEATURE_ORIT):
-                        insdc_feature_type = bc.INSDC_FEATURE_ORIGIN_TRANSFER
-                    else:
-                        insdc_feature_type = bc.INSDC_FEATURE_ORIGIN_REPLICTION
-                else:
+                if('db_xrefs' in feature):
                     qualifiers['db_xref'] = feature['db_xrefs']
+                if('product' in feature):
                     qualifiers['product'] = feature['product']
-                    if(feature['type'] == bc.FEATURE_CDS) or (feature_type == bc.FEATURE_SORF):
-                        qualifiers['locus_tag'] = feature['locus']
-                        qualifiers['translation'] = feature['sequence']
-                        qualifiers['codon_start'] = 1
-                        insdc_feature_type = bc.INSDC_FEATURE_CDS
-                        inference = []
-                        if(feature['type'] == bc.FEATURE_CDS):
-                            inference.append('ab initio prediction:Prodigal:2.6')
-                        else:
-                            inference.append('ab initio prediction:Bakta')
-                        if('hypothetical' not in feature):
-                            if('ups' in feature):
-                                if('ncbi_nrp_id' in feature['ups']):
-                                    protein_id = feature['ups']['ncbi_nrp_id']
-                                    qualifiers['protein_id'] = protein_id
-                            if('psc' in feature):
-                                if('uniref90_id' in feature['psc']):
-                                    psc_subject_id = feature['psc']['uniref90_id']
-                                    inference.append(f'similar to AA sequence:UniProtKB:{psc_subject_id}')
-                            if('ips' in feature):
-                                if('uniref100_id' in feature['ips']):
-                                    ips_subject_id = feature['ips']['uniref100_id']
-                                    if(ips_subject_id != psc_subject_id):
-                                        inference.append(f'similar to AA sequence:UniProtKB:{ips_subject_id}')
-                        qualifiers['inference'] = inference
-                    elif(feature_type == bc.FEATURE_T_RNA):
-                        # TODO: Position anticodon
-                        qualifiers['locus_tag'] = feature['locus']
-                        if('notes' in feature):
-                            if('anti_codon' in feature):
-                                qualifiers['note'] = feature['notes']
-                                qualifiers['anticodon'] = f"(aa:{feature['notes'][-10:-7]},seq:{feature['anti_codon'].lower()})"
-                            else:
-                                qualifiers['anticodon'] = f"(seq:{feature['anti_codon'].lower()})"
-                        qualifiers['inference'] = 'profile:tRNAscan:2.0'
-                        insdc_feature_type = bc.INSDC_FEATURE_T_RNA
-                    elif(feature_type == bc.FEATURE_TM_RNA):
-                        qualifiers['locus_tag'] = feature['locus']
-                        qualifiers['inference'] = 'profile:aragorn:1.2'
-                        insdc_feature_type = bc.INSDC_FEATURE_TM_RNA
-                    elif(feature_type == bc.FEATURE_R_RNA):
-                        for reference in feature['db_xrefs']:
-                            if(reference.split(':')[0] == 'RFAM'):
-                                r_subject_id = reference.split(':')[1]
-                        qualifiers['locus_tag'] = feature['locus']
-                        qualifiers['inference'] = f'profile:Rfam:{r_subject_id}'
-                        insdc_feature_type = bc.INSDC_FEATURE_R_RNA
-                    elif(feature_type == bc.FEATURE_NC_RNA):
-                        # TODO: ncRNA_class
-                        for reference in feature['db_xrefs']:
-                            if(reference.split(':')[0] == 'RFAM'):
-                                nc_subject_id = reference.split(':')[1]
-                        qualifiers['locus_tag'] = feature['locus']
-                        qualifiers['ncRNA_class'] = 'other'
-                        qualifiers['inference'] = f'profile:Rfam:{nc_subject_id}'
-                        insdc_feature_type = bc.INSDC_FEATURE_NC_RNA
-                    elif(feature_type==bc.FEATURE_NC_RNA_REGION):
-                        for reference in feature['db_xrefs']:
-                            if(reference.split(':')[0] == 'RFAM'):
-                                nc_subject_id = reference.split(':')[1]
-                        qualifiers['locus_tag'] = feature['locus']
-                        qualifiers['ncRNA_class'] = 'other'
-                        qualifiers['inference'] = f'profile:Rfam:{nc_subject_id}'
-                        insdc_feature_type = bc.INSDC_FEATURE_REGULATORY
-                    elif(feature_type==bc.FEATURE_CRISPR):
-                        qualifiers['repeats'] = feature['repeats']
-                        qualifiers['repeat_consensus'] = feature['repeat_consensus']
-                        qualifiers['repeat_length'] = feature['repeat_length']
-                        qualifiers['spacer_length'] = feature['spacer_length']
-                        feature['type'] = 'misc_feature'
-                        insdc_feature_type = bc.INSDC_FEATURE_MISC_FEAURE
+                if('locus' in feature):
+                    qualifiers['locus_tag'] = feature['locus']
+                
+                if(feature['type'] == bc.FEATURE_GAP):
+                    insdc_feature_type = bc.INSDC_FEATURE_GAP
+                    qualifiers['estimated_length'] = feature['length']
+                elif(feature['type'] == bc.FEATURE_ORIC or feature['type'] == bc.FEATURE_ORIV):
+                    #TODO: Add fuzzy positions for oriC/oriV
+                    insdc_feature_type = bc.INSDC_FEATURE_ORIGIN_REPLICTION
+                elif(feature['type'] == feature['type'] == bc.FEATURE_ORIT):
+                    #TODO: Add fuzzy positions for oriT
+                    insdc_feature_type = bc.INSDC_FEATURE_ORIGIN_TRANSFER
+                elif(feature['type'] == bc.FEATURE_CDS) or (feature['type'] == bc.FEATURE_SORF):
+                    qualifiers['translation'] = feature['sequence']
+                    qualifiers['codon_start'] = 1
+                    insdc_feature_type = bc.INSDC_FEATURE_CDS
+                    inference = []
+                    inference.append('ab initio prediction:Prodigal:2.6' if feature['type'] == bc.FEATURE_CDS else 'ab initio prediction:Bakta')
+                    if('hypothetical' not in feature):
+                        if('ups' in feature):
+                            if('ncbi_nrp_id' in feature['ups']):
+                                qualifiers['protein_id'] = feature['ups']['ncbi_nrp_id']
+                        if('ips' in feature):
+                            if('uniref100_id' in feature['ips']):
+                                ips_subject_id = feature['ips']['uniref100_id']
+                                inference.append(f'similar to AA sequence:UniProtKB:{ips_subject_id}')
+                        if('psc' in feature):
+                            if('uniref90_id' in feature['psc']):
+                                psc_subject_id = feature['psc']['uniref90_id']
+                                inference.append(f'similar to AA sequence:UniProtKB:{psc_subject_id}')
+                    qualifiers['inference'] = inference
+                elif(feature['type'] == bc.FEATURE_T_RNA):
+                    # TODO: Position anticodon
+                    if('notes' in feature):
+                        if('anti_codon' in feature):
+                            qualifiers['note'] = feature['notes']
+                            i = feature['product'].find('-')
+                            t_rna_type = feature['product'][i+1:].lower()
+                            qualifiers['anticodon'] = f"(aa:{t_rna_type},seq:{feature['anti_codon'].lower()})"
+                    qualifiers['inference'] = 'profile:tRNAscan:2.0'
+                    insdc_feature_type = bc.INSDC_FEATURE_T_RNA
+                elif(feature['type'] == bc.FEATURE_TM_RNA):
+                    qualifiers['inference'] = 'profile:aragorn:1.2'
+                    insdc_feature_type = bc.INSDC_FEATURE_TM_RNA
+                elif(feature['type'] == bc.FEATURE_R_RNA):
+                    for reference in feature['db_xrefs']:
+                        if(reference.split(':')[0] == 'RFAM'):
+                            r_subject_id = reference.split(':')[1]
+                    qualifiers['inference'] = f'profile:Rfam:{r_subject_id}'
+                    insdc_feature_type = bc.INSDC_FEATURE_R_RNA
+                elif(feature['type'] == bc.FEATURE_NC_RNA):
+                    # TODO: ncRNA_class
+                    for reference in feature['db_xrefs']:
+                        if(reference.split(':')[0] == 'RFAM'):
+                            nc_subject_id = reference.split(':')[1]
+                    qualifiers['ncRNA_class'] = 'other'
+                    qualifiers['inference'] = f'profile:Rfam:{nc_subject_id}'
+                    insdc_feature_type = bc.INSDC_FEATURE_NC_RNA
+                elif(feature['type']==bc.FEATURE_NC_RNA_REGION):
+                    for reference in feature['db_xrefs']:
+                        if(reference.split(':')[0] == 'RFAM'):
+                            nc_subject_id = reference.split(':')[1]
+                    qualifiers['ncRNA_class'] = 'other'
+                    qualifiers['inference'] = f'profile:Rfam:{nc_subject_id}'
+                    insdc_feature_type = bc.INSDC_FEATURE_REGULATORY
+                elif(feature['type']==bc.FEATURE_CRISPR):
+                    qualifiers['repeats'] = feature['repeats']
+                    qualifiers['repeat_consensus'] = feature['repeat_consensus']
+                    qualifiers['repeat_length'] = feature['repeat_length']
+                    qualifiers['spacer_length'] = feature['spacer_length']
+                    feature['type'] = 'misc_feature'
+                    insdc_feature_type = bc.INSDC_FEATURE_MISC_FEATURE
 
-                strand_dir = +1 if feature['strand'] == bc.STRAND_FORWARD else -1
+                strand_dir = -1 if feature['strand'] == bc.STRAND_REVERSE else +1
 
-                if('gene' in feature and feature['type'] != bc.FEATURE_NC_RNA_REGION):
+                if(feature.get('gene', '') and feature['type'] != bc.FEATURE_NC_RNA_REGION):
                     qualifiers['gene'] = feature['gene']
                     gene_qualifier = {
                         'gene': feature['gene'],
