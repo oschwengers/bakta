c84b4e1ae4c3e79aee6e3010538986646f1b369a
Oliver Schwengers <oliver.schwengers@computational.bio.uni-giessen.de>
2020-11-30 17:35:10 +0100

    add locus tag to tsv output

--- bakta/io/tsv.py
+++ bakta/io/tsv.py
@@ -29,7 +29,7 @@ def write_tsv(contigs, features_by_contig, tsv_path):
     with tsv_path.open('w') as fh:
         fh.write(f'#Annotated with Bakta v{bakta.__version__}, https://github.com/oschwengers/bakta\n')
         fh.write(f"#Database v{cfg.db_info['major']}.{cfg.db_info['minor']}, https://doi.org/10.5281/zenodo.4247252\n")
-        fh.write('#Sequence Id\tType\tStart\tStop\tStrand\tGene\tProduct\tDbXrefs\n')
+        fh.write('#Sequence Id\tType\tStart\tStop\tStrand\tLocus Tag\tGene\tProduct\tDbXrefs\n')
         for contig in contigs:
             for feat in features_by_contig[contig['id']]:
                 feat_type = feat['type']
@@ -37,6 +37,6 @@ def write_tsv(contigs, features_by_contig, tsv_path):
                     feat_type = bc.INSDC_FEATURE_ASSEMBLY_GAP if feat['length'] >= 100 else bc.INSDC_FEATURE_GAP
                 
                 gene = feat['gene'] if feat.get('gene', None) else ''
-                fh.write('\t'.join([feat['contig'], feat_type, str(feat['start']), str(feat['stop']), feat['strand'], gene, feat.get('product', ''), ', '.join(sorted(feat.get('db_xrefs', [])))]))
+                fh.write('\t'.join([feat['contig'], feat_type, str(feat['start']), str(feat['stop']), feat['strand'], feat.get('locus', ''), gene, feat.get('product', ''), ', '.join(sorted(feat.get('db_xrefs', [])))]))
                 fh.write('\n')
     return
