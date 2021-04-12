cc061a0338234ae2fd3bc287e1e01276a4b88340
AnnaRehm <anna.rehm@bioinfsys.uni-giessen.de>
2021-04-12 11:55:23 +0200

    Fix tests to work on new check_version(). #21

--- test/test_dependencies.py
+++ test/test_dependencies.py
@@ -7,125 +7,125 @@ from bakta import utils as bu
 
 def test_major():
     # meeting conditions without max version
-    assert bu.check_version(bu.Version(1,0,0), 3, bu.Version(1,0,0), bu.Version(None), 'foo') == True
-    assert bu.check_version(bu.Version(1,0), 2, bu.Version(1,0,0), bu.Version(None), 'foo') == True
-    assert bu.check_version(bu.Version(1), 1, bu.Version(1,0,0), bu.Version(None), 'foo') == True
+    assert bu.check_version(bu.Version(1,0,0), bu.Version(1,0,0), bu.Version(bu.Max_version,bu.Max_version,bu.Max_version), 'foo') == True
+    assert bu.check_version(bu.Version(1,0), bu.Version(1,0,0), bu.Version(bu.Max_version,bu.Max_version,bu.Max_version), 'foo') == True
+    assert bu.check_version(bu.Version(1), bu.Version(1,0,0), bu.Version(bu.Max_version,bu.Max_version,bu.Max_version), 'foo') == True
 
     # meeting conditions without min version
-    assert bu.check_version(bu.Version(1,0,0), 3, bu.Version(None), bu.Version(1,0,0), 'foo') == True
-    assert bu.check_version(bu.Version(1,0), 2, bu.Version(None), bu.Version(1,0,0), 'foo') == True
-    assert bu.check_version(bu.Version(1), 1, bu.Version(None), bu.Version(1,0,0), 'foo') == True
+    assert bu.check_version(bu.Version(1,0,0), bu.Version(bu.Min_version,bu.Min_version,bu.Min_version), bu.Version(1,0,0), 'foo') == True
+    assert bu.check_version(bu.Version(1,0), bu.Version(bu.Min_version,bu.Min_version,bu.Min_version), bu.Version(1,0,0), 'foo') == True
+    assert bu.check_version(bu.Version(1), bu.Version(bu.Min_version,bu.Min_version,bu.Min_version), bu.Version(1,0,0), 'foo') == True
 
     # meeting conditions on lower bound
-    assert bu.check_version(bu.Version(1,0,0), 3, bu.Version(1,0,0), bu.Version(1,0,0), 'foo') == True
-    assert bu.check_version(bu.Version(1,0), 2, bu.Version(1,0,0), bu.Version(1,0), 'foo') == True
-    assert bu.check_version(bu.Version(1), 1, bu.Version(1,0,0), bu.Version(1), 'foo') == True
+    assert bu.check_version(bu.Version(1,0,0), bu.Version(1,0,0), bu.Version(1,0,0), 'foo') == True
+    assert bu.check_version(bu.Version(1,0), bu.Version(1,0,0), bu.Version(1,0), 'foo') == True
+    assert bu.check_version(bu.Version(1), bu.Version(1,0,0), bu.Version(1), 'foo') == True
     
     # meeting conditions on upper bound
-    assert bu.check_version(bu.Version(2,2,2), 3, bu.Version(1,0,0), bu.Version(2,2,2), 'foo') == True
-    assert bu.check_version(bu.Version(2,2), 2, bu.Version(1,1), bu.Version(2,2), 'foo') == True
-    assert bu.check_version(bu.Version(2), 1, bu.Version(1), bu.Version(2), 'foo') == True
+    assert bu.check_version(bu.Version(2,2,2), bu.Version(1,0,0), bu.Version(2,2,2), 'foo') == True
+    assert bu.check_version(bu.Version(2,2), bu.Version(1,1), bu.Version(2,2), 'foo') == True
+    assert bu.check_version(bu.Version(2), bu.Version(1), bu.Version(2), 'foo') == True
     
     # meeting conditions in between
-    assert bu.check_version(bu.Version(1,0,1), 3, bu.Version(1,0,0), bu.Version(2,0,0), 'foo') == True
-    assert bu.check_version(bu.Version(1,0,100), 3, bu.Version(1,0,0), bu.Version(2,0,0), 'foo') == True
-    assert bu.check_version(bu.Version(1,1,0), 3, bu.Version(1,0,0), bu.Version(2,0,0), 'foo') == True
-    assert bu.check_version(bu.Version(1,100,0), 3, bu.Version(1,0,0), bu.Version(2,0,0), 'foo') == True
+    assert bu.check_version(bu.Version(1,0,1), bu.Version(1,0,0), bu.Version(2,0,0), 'foo') == True
+    assert bu.check_version(bu.Version(1,0,100), bu.Version(1,0,0), bu.Version(2,0,0), 'foo') == True
+    assert bu.check_version(bu.Version(1,1,0), bu.Version(1,0,0), bu.Version(2,0,0), 'foo') == True
+    assert bu.check_version(bu.Version(1,100,0), bu.Version(1,0,0), bu.Version(2,0,0), 'foo') == True
 
-    assert bu.check_version(bu.Version(1,1), 2, bu.Version(1,0), bu.Version(2,0), 'foo') == True
-    assert bu.check_version(bu.Version(1,100), 2, bu.Version(1,0), bu.Version(2,0), 'foo') == True
-    assert bu.check_version(bu.Version(1,1), 2, bu.Version(1,0), bu.Version(2,0), 'foo') == True
-    assert bu.check_version(bu.Version(1,100), 2, bu.Version(1,0), bu.Version(2,0), 'foo') == True
+    assert bu.check_version(bu.Version(1,1), bu.Version(1,0), bu.Version(2,0), 'foo') == True
+    assert bu.check_version(bu.Version(1,100), bu.Version(1,0), bu.Version(2,0), 'foo') == True
+    assert bu.check_version(bu.Version(1,1), bu.Version(1,0), bu.Version(2,0), 'foo') == True
+    assert bu.check_version(bu.Version(1,100), bu.Version(1,0), bu.Version(2,0), 'foo') == True
     
     # meeting conditions in between
-    assert bu.check_version(bu.Version(2,1,1), 3, bu.Version(1,2,10), bu.Version(3,0,0), 'foo') == True
-    assert bu.check_version(bu.Version(2,2,1), 3, bu.Version(1,2,10), bu.Version(3,0,0), 'foo') == True
+    assert bu.check_version(bu.Version(2,1,1), bu.Version(1,2,10), bu.Version(3,0,0), 'foo') == True
+    assert bu.check_version(bu.Version(2,2,1), bu.Version(1,2,10), bu.Version(3,0,0), 'foo') == True
     
     
     # unmeeting major conditions without max version
-    assert bu.check_version(bu.Version(1,0,0), 3, bu.Version(2,0,0), bu.Version(None), 'foo') == False
-    assert bu.check_version(bu.Version(1,0), 2, bu.Version(2,0), bu.Version(None), 'foo') == False
-    assert bu.check_version(bu.Version(1), 1, bu.Version(2), bu.Version(None), 'foo') == False
+    assert bu.check_version(bu.Version(1,0,0), bu.Version(2,0,0), bu.Version(bu.Max_version,bu.Max_version,bu.Max_version), 'foo') == False
+    assert bu.check_version(bu.Version(1,0), bu.Version(2,0), bu.Version(bu.Max_version,bu.Max_version,bu.Max_version), 'foo') == False
+    assert bu.check_version(bu.Version(1), bu.Version(2), bu.Version(bu.Max_version,bu.Max_version,bu.Max_version), 'foo') == False
 
     # unmeeting major conditions without min version
-    assert bu.check_version(bu.Version(2,0,0), 3, bu.Version(None), bu.Version(1,0,0), 'foo') == False
-    assert bu.check_version(bu.Version(2,0), 2, bu.Version(None), bu.Version(1,0,0), 'foo') == False
-    assert bu.check_version(bu.Version(2), 1, bu.Version(None), bu.Version(1,0,0), 'foo') == False
+    assert bu.check_version(bu.Version(2,0,0), bu.Version(bu.Min_version,bu.Min_version,bu.Min_version), bu.Version(1,0,0), 'foo') == False
+    assert bu.check_version(bu.Version(2,0), bu.Version(bu.Min_version,bu.Min_version,bu.Min_version), bu.Version(1,0,0), 'foo') == False
+    assert bu.check_version(bu.Version(2), bu.Version(bu.Min_version,bu.Min_version,bu.Min_version), bu.Version(1,0,0), 'foo') == False
 
     # unmeeting major conditions on lower bound
-    assert bu.check_version(bu.Version(1,0,0), 3, bu.Version(2,0,0), bu.Version(3,0,0), 'foo') == False
-    assert bu.check_version(bu.Version(1,0), 2, bu.Version(2,0), bu.Version(3,0), 'foo') == False
-    assert bu.check_version(bu.Version(1), 1, bu.Version(2), bu.Version(3), 'foo') == False
+    assert bu.check_version(bu.Version(1,0,0), bu.Version(2,0,0), bu.Version(3,0,0), 'foo') == False
+    assert bu.check_version(bu.Version(1,0), bu.Version(2,0), bu.Version(3,0), 'foo') == False
+    assert bu.check_version(bu.Version(1), bu.Version(2), bu.Version(3), 'foo') == False
     
     # unmeeting major conditions on upper bound
-    assert bu.check_version(bu.Version(3,0,0), 3, bu.Version(1,0,0), bu.Version(2,0,0), 'foo') == False
-    assert bu.check_version(bu.Version(3,0), 2, bu.Version(1,1), bu.Version(2,0), 'foo') == False
-    assert bu.check_version(bu.Version(3), 1, bu.Version(1), bu.Version(2), 'foo') == False
+    assert bu.check_version(bu.Version(3,0,0), bu.Version(1,0,0), bu.Version(2,0,0), 'foo') == False
+    assert bu.check_version(bu.Version(3,0), bu.Version(1,1), bu.Version(2,0), 'foo') == False
+    assert bu.check_version(bu.Version(3), bu.Version(1), bu.Version(2), 'foo') == False
 
 
 def test_minor():
     # meeting conditions without max version
-    assert bu.check_version(bu.Version(1,10,0), 3, bu.Version(1,1,0), bu.Version(None), 'foo') == True
-    assert bu.check_version(bu.Version(1,10), 2, bu.Version(1,1,0), bu.Version(None), 'foo') == True
+    assert bu.check_version(bu.Version(1,10,0), bu.Version(1,1,0), bu.Version(bu.Max_version,bu.Max_version,bu.Max_version), 'foo') == True
+    assert bu.check_version(bu.Version(1,10), bu.Version(1,1,0), bu.Version(bu.Max_version,bu.Max_version,bu.Max_version), 'foo') == True
 
     # meeting conditions without min version
-    assert bu.check_version(bu.Version(1,1,0), 3, bu.Version(None), bu.Version(1,10,0), 'foo') == True
-    assert bu.check_version(bu.Version(1,1), 2, bu.Version(None), bu.Version(1,10,0), 'foo') == True
+    assert bu.check_version(bu.Version(1,1,0), bu.Version(bu.Min_version,bu.Min_version,bu.Min_version), bu.Version(1,10,0), 'foo') == True
+    assert bu.check_version(bu.Version(1,1), bu.Version(bu.Min_version,bu.Min_version,bu.Min_version), bu.Version(1,10,0), 'foo') == True
 
     # meeting conditions on lower bound
-    assert bu.check_version(bu.Version(1,0,0), 3, bu.Version(1,0,0), bu.Version(1,10,0), 'foo') == True
-    assert bu.check_version(bu.Version(1,0), 2, bu.Version(1,0), bu.Version(1,10), 'foo') == True
+    assert bu.check_version(bu.Version(1,0,0), bu.Version(1,0,0), bu.Version(1,10,0), 'foo') == True
+    assert bu.check_version(bu.Version(1,0), bu.Version(1,0), bu.Version(1,10), 'foo') == True
     
     # meeting conditions on upper bound
-    assert bu.check_version(bu.Version(1,10,0), 3, bu.Version(1,0,0), bu.Version(1,10,0), 'foo') == True
-    assert bu.check_version(bu.Version(1,10), 2, bu.Version(1,0), bu.Version(1,10), 'foo') == True
+    assert bu.check_version(bu.Version(1,10,0), bu.Version(1,0,0), bu.Version(1,10,0), 'foo') == True
+    assert bu.check_version(bu.Version(1,10), bu.Version(1,0), bu.Version(1,10), 'foo') == True
     
     # meeting conditions in between
-    assert bu.check_version(bu.Version(1,5,0), 3, bu.Version(1,0,0), bu.Version(1,10,0), 'foo') == True
+    assert bu.check_version(bu.Version(1,5,0), bu.Version(1,0,0), bu.Version(1,10,0), 'foo') == True
     
 
     # unmeeting major conditions without max version
-    assert bu.check_version(bu.Version(1,0,0), 3, bu.Version(1,10,0), bu.Version(None), 'foo') == False
-    assert bu.check_version(bu.Version(1,0), 2, bu.Version(1,10), bu.Version(None), 'foo') == False
+    assert bu.check_version(bu.Version(1,0,0), bu.Version(1,10,0), bu.Version(bu.Max_version,bu.Max_version,bu.Max_version), 'foo') == False
+    assert bu.check_version(bu.Version(1,0), bu.Version(1,10), bu.Version(bu.Max_version,bu.Max_version,bu.Max_version), 'foo') == False
 
     # unmeeting major conditions without min version
-    assert bu.check_version(bu.Version(1,10,0), 3, bu.Version(None), bu.Version(1,0,0), 'foo') == False
-    assert bu.check_version(bu.Version(1,10), 2, bu.Version(None), bu.Version(1,0), 'foo') == False
+    assert bu.check_version(bu.Version(1,10,0), bu.Version(bu.Min_version,bu.Min_version,bu.Min_version), bu.Version(1,0,0), 'foo') == False
+    assert bu.check_version(bu.Version(1,10), bu.Version(bu.Min_version,bu.Min_version,bu.Min_version), bu.Version(1,0), 'foo') == False
 
     # unmeeting major conditions on lower bound
-    assert bu.check_version(bu.Version(1,0,0), 3, bu.Version(1,10,0), bu.Version(1,0,0), 'foo') == False
-    assert bu.check_version(bu.Version(1,0), 2, bu.Version(1,10), bu.Version(1,0), 'foo') == False
+    assert bu.check_version(bu.Version(1,0,0), bu.Version(1,10,0), bu.Version(1,0,0), 'foo') == False
+    assert bu.check_version(bu.Version(1,0), bu.Version(1,10), bu.Version(1,0), 'foo') == False
     
     # unmeeting major conditions on upper bound
-    assert bu.check_version(bu.Version(1,10,0), 3, bu.Version(1,0,0), bu.Version(1,5,0), 'foo') == False
-    assert bu.check_version(bu.Version(1,10), 2, bu.Version(1,0), bu.Version(1,5), 'foo') == False
+    assert bu.check_version(bu.Version(1,10,0), bu.Version(1,0,0), bu.Version(1,5,0), 'foo') == False
+    assert bu.check_version(bu.Version(1,10), bu.Version(1,0), bu.Version(1,5), 'foo') == False
 
 
 def test_patch():
     # meeting conditions without max version
-    assert bu.check_version(bu.Version(1,0,10), 3, bu.Version(1,0,1), bu.Version(None), 'foo') == True
+    assert bu.check_version(bu.Version(1,0,10), bu.Version(1,0,1), bu.Version(bu.Max_version,bu.Max_version,bu.Max_version), 'foo') == True
 
     # meeting conditions without min version
-    assert bu.check_version(bu.Version(1,0,10), 3, bu.Version(None), bu.Version(1,0,10), 'foo') == True
+    assert bu.check_version(bu.Version(1,0,10), bu.Version(bu.Min_version,bu.Min_version,bu.Min_version), bu.Version(1,0,10), 'foo') == True
 
     # meeting conditions on lower bound
-    assert bu.check_version(bu.Version(1,0,1), 3, bu.Version(1,0,1), bu.Version(1,0,10), 'foo') == True
+    assert bu.check_version(bu.Version(1,0,1), bu.Version(1,0,1), bu.Version(1,0,10), 'foo') == True
     
     # meeting conditions on upper bound
-    assert bu.check_version(bu.Version(1,0,10), 3, bu.Version(1,0,0), bu.Version(1,0,10), 'foo') == True
+    assert bu.check_version(bu.Version(1,0,10), bu.Version(1,0,0), bu.Version(1,0,10), 'foo') == True
     
     # meeting conditions in between
-    assert bu.check_version(bu.Version(1,0,5), 3, bu.Version(1,0,1), bu.Version(1,0,10), 'foo') == True
+    assert bu.check_version(bu.Version(1,0,5), bu.Version(1,0,1), bu.Version(1,0,10), 'foo') == True
     
 
     # unmeeting major conditions without max version
-    assert bu.check_version(bu.Version(1,0,0), 3, bu.Version(1,0,10), bu.Version(None), 'foo') == False
+    assert bu.check_version(bu.Version(1,0,0), bu.Version(1,0,10), bu.Version(bu.Max_version,bu.Max_version,bu.Max_version), 'foo') == False
 
     # unmeeting major conditions without min version
-    assert bu.check_version(bu.Version(1,0,10), 3, bu.Version(None), bu.Version(1,0,0), 'foo') == False
+    assert bu.check_version(bu.Version(1,0,10), bu.Version(bu.Min_version,bu.Min_version,bu.Min_version), bu.Version(1,0,0), 'foo') == False
 
     # unmeeting major conditions on lower bound
-    assert bu.check_version(bu.Version(1,0,0), 3, bu.Version(1,0,10), bu.Version(1,0,10), 'foo') == False
+    assert bu.check_version(bu.Version(1,0,0), bu.Version(1,0,10), bu.Version(1,0,10), 'foo') == False
     
     # unmeeting major conditions on upper bound
-    assert bu.check_version(bu.Version(1,0,10), 3, bu.Version(1,0,0), bu.Version(1,0,5), 'foo') == False
+    assert bu.check_version(bu.Version(1,0,10), bu.Version(1,0,0), bu.Version(1,0,5), 'foo') == False
