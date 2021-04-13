c186fa569c30a52c54697849fb229a65fdd27532
Oliver Schwengers <oliver.schwengers@computational.bio.uni-giessen.de>
2021-04-13 23:58:47 +0200

    fine tune thread pool logic

--- bakta/features/s_orf.py
+++ bakta/features/s_orf.py
@@ -125,11 +125,13 @@ def overlap_filter(genome, orfs_raw):
         for contig in genome['contigs']:
             contig_sorfs = sorfs_per_contig[contig['id']]
             log.debug('filter: contig=%s, # sORFs=%i', contig['id'], len(contig_sorfs))
-            if(len(contig_sorfs) < 1000):  # execute sORF filter tasks
+            if(len(contig_sorfs) < 100):  # execute sORF filter tasks
                 sorf_keys = filter_sorf(contig_sorfs, cdss_per_contig[contig['id']], r_rna_per_contig[contig['id']], t_rnas_per_contig[contig['id']], crispr_arrays_per_contig[contig['id']])
                 for sorf_key in [sk for sk in sorf_keys if sk is not None]:
                     discarded_sorf_keys.add(sorf_key)
-            else:  # submit sORF filter tasks to thread pool
+            elif(len(contig_sorfs) < 1000):  # submit sORF filter task to thread pool
+                futures.append(tpe.submit(filter_sorf, sorf_chunk, cdss_per_contig[contig['id']], r_rna_per_contig[contig['id']], t_rnas_per_contig[contig['id']], crispr_arrays_per_contig[contig['id']]))
+            else:  # submit sORF chunk filter tasks to thread pool
                 chunk_size = math.ceil(len(contig_sorfs) / cfg.threads) if (len(contig_sorfs) >= cfg.threads * 1000) else 1000
                 log.debug('filter: chunk-size=%i', chunk_size)
                 for i in range(0, len(contig_sorfs), chunk_size):
