9baa994f87ad9a3b6ddae822daf4882c6701faba
Oliver Schwengers <oliver.schwengers@computational.bio.uni-giessen.de>
2021-04-19 17:51:37 +0200

    refactor arg tests

--- test/test_args.py
+++ test/test_args.py
@@ -20,8 +20,9 @@ FILES = [
 @pytest.mark.parametrize(
     "cmd_line",
     [
-        (["bin/bakta", '--db', 'test/db']),  # no provided
-        (["bin/bakta", '--db', 'test/db', 'foo.fasta'])  # non-existing
+        (["bin/bakta", '--db', 'test/db']),  # not provided
+        (["bin/bakta", '--db', 'test/db', '']),  # empty
+        (["bin/bakta", '--db', 'test/db', 'foo.fasta'])  # not existing
     ]
 )
 def test_genome_failing(cmd_line):
@@ -33,11 +34,12 @@ def test_genome_failing(cmd_line):
 @pytest.mark.parametrize(
     "cmd_line,env_key,env_value",
     [
-        (["bin/bakta", 'test/data/NC_002127.1.fna'], '', ''),  # no provided
-        (["bin/bakta", '--db', 'test/data/NC_002127.1.fna'], '', ''),  # parameter missing path
-        (["bin/bakta", '--db', 'test/foo', 'test/data/NC_002127.1.fna'], '', ''),  # parameter wrong path
+        (["bin/bakta", 'test/data/NC_002127.1.fna'], 'foo', ''),  # not provided
+        (["bin/bakta", '--db', 'test/data/NC_002127.1.fna'], 'foo', ''),  # parameter missing path
+        (["bin/bakta", '--db', '', 'test/data/NC_002127.1.fna'], 'foo', ''),  # parameter empty
+        (["bin/bakta", '--db', 'test/foo', 'test/data/NC_002127.1.fna'], 'foo', ''),  # parameter not existing
         (["bin/bakta", 'test/data/NC_002127.1.fna'], 'BAKTA_DB', ''),  # environment missing path
-        (["bin/bakta", 'test/data/NC_002127.1.fna'], 'BAKTA_DB', 'test/foo'),  # environment wrong path
+        (["bin/bakta", 'test/data/NC_002127.1.fna'], 'BAKTA_DB', 'test/foo')  # environment not existing
     ]
 )
 def test_database_failing(cmd_line, env_key, env_value):
@@ -56,23 +58,32 @@ def test_database_ok(tmpdir):
     proc = run(["bin/bakta", '--db', 'test/db', '--output', tmpdir, '--skip-tmrna', '--skip-trna', '--skip-rrna', '--skip-ncrna', '--skip-ncrna-region', '--skip-crispr', '--skip-cds', '--skip-sorf', '--skip-ori', '--skip-gap', 'test/data/NC_002127.1.fna'])
     assert proc.returncode == 0
 
-    # parameter OK
+    # environment OK
     env = os.environ
     env['BAKTA_DB'] = 'test/db'
     proc = run(["bin/bakta", '--output', tmpdir, '--skip-tmrna', '--skip-trna', '--skip-rrna', '--skip-ncrna', '--skip-ncrna-region', '--skip-crispr', '--skip-cds', '--skip-sorf', '--skip-ori', '--skip-gap', 'test/data/NC_002127.1.fna'], env=env)
     assert proc.returncode == 0
 
 
-def test_prodigal_tf(tmpdir):
+def test_prodigal_tf_failiing(tmpdir):
     # test prodigal training file arguments
 
-    # missing
+    # missing path
     proc = run(["bin/bakta", '--db', 'test/db', '--output', tmpdir, '--prefix', 'test', '--prodigal-tf', 'test/data/NC_002127.1.fna'])
     assert proc.returncode != 0
 
-    # non-existing
+    # empty
+    proc = run(["bin/bakta", '--db', 'test/db', '--output', tmpdir, '--prefix', 'test', '--prodigal-tf', '', 'test/data/foo', 'test/data/NC_002127.1.fna'])
+    assert proc.returncode != 0
+
+    # not existing
     proc = run(["bin/bakta", '--db', 'test/db', '--output', tmpdir, '--prefix', 'test', '--prodigal-tf', 'test/data/foo', 'test/data/NC_002127.1.fna'])
     assert proc.returncode != 0
+
+
+@pytest.mark.slow
+def test_prodigal_tf_ok(tmpdir):
+    # test prodigal training file arguments
 
     # OK
     proc = run(["bin/bakta", '--db', 'test/db', '--output', tmpdir, '--prefix', 'test', '--prodigal-tf', 'test/data/prodigal.tf', '--skip-trna', '--skip-rrna', '--skip-ncrna', '--skip-ncrna-region', '--skip-crispr', '--skip-sorf', '--skip-ori', '--skip-gap', 'test/data/NC_002127.1.fna'])
