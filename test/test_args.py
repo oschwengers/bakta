18557734addc73d998904b93ad11c2fdbb3b9521
Oliver Schwengers <oliver.schwengers@computational.bio.uni-giessen.de>
2021-04-19 14:58:23 +0200

    parameterize tests

--- test/test_args.py
+++ test/test_args.py
@@ -1,51 +1,64 @@
 import os
+import pytest
+
 from pathlib import Path
 from subprocess import run
 
 
-def test_genome():
+FILES = [
+    'test.log',
+    'test.json',
+    'test.tsv',
+    'test.gff3',
+    'test.gbff',
+    'test.embl',
+    'test.fna',
+    'test.faa'
+]
+
+
+@pytest.mark.parametrize(
+    "cmd_line",
+    [
+        (["bin/bakta", '--db', 'test/db']),  # no provided
+        (["bin/bakta", '--db', 'test/db', 'foo.fasta'])  # non-existing
+    ]
+)
+def test_genome_failing(cmd_line):
     # test genome arguments
-
-    # no provided
-    proc = run(["bin/bakta", '--db', 'test/db'])
-    assert proc.returncode != 0
-
-    # non-existing
-    proc = run(["bin/bakta", '--db', 'test/db', 'foo.fasta'])
+    proc = run(cmd_line)
     assert proc.returncode != 0
 
 
-def test_database(tmpdir):
+@pytest.mark.parametrize(
+    "cmd_line,env_key,env_value",
+    [
+        (["bin/bakta", 'test/data/NC_002127.1.fna'], '', ''),  # no provided
+        (["bin/bakta", '--db', 'test/db', 'foo.fasta'], '', ''),  # non-existing
+        (["bin/bakta", '--db', 'test/data/NC_002127.1.fna'], '', ''),  # parameter missing path
+        (["bin/bakta", '--db', 'test/', 'test/data/NC_002127.1.fna'], '', ''),  # parameter wrong path
+        (["bin/bakta", '--db', 'test/', 'test/data/NC_002127.1.fna'], 'BAKTA_DB', ''),  # environment missing path
+        (["bin/bakta", '--db', 'test/', 'test/data/NC_002127.1.fna'], 'BAKTA_DB', 'test/'),  # parameter wrong path
+    ]
+)
+def test_database_failing(cmd_line, env_key, env_value):
     # test database arguments
 
-    # no provided
-    proc = run(["bin/bakta", 'test/data/NC_002127.1.fna'])
+    env = os.environ
+    env[env_key] = env_value
+    proc = run(cmd_line, env=env)
     assert proc.returncode != 0
 
-    # parameter missing path
-    proc = run(["bin/bakta", '--db', 'test/data/NC_002127.1.fna'])
-    assert proc.returncode != 0
 
-    # parameter wrong path
-    proc = run(["bin/bakta", '--db', 'test/', 'test/data/NC_002127.1.fna'])
-    assert proc.returncode != 0
+def test_database_ok(tmpdir):
+    # test database arguments
 
     # parameter OK
     proc = run(["bin/bakta", '--db', 'test/db', '--output', tmpdir, '--skip-tmrna', '--skip-trna', '--skip-rrna', '--skip-ncrna', '--skip-ncrna-region', '--skip-crispr', '--skip-cds', '--skip-sorf', '--skip-ori', '--skip-gap', 'test/data/NC_002127.1.fna'])
     assert proc.returncode == 0
 
-    # environment missing path
-    env = os.environ
-    env['BAKTA_DB'] = ''
-    proc = run(["bin/bakta", 'test/data/NC_002127.1.fna'], env=env)
-    assert proc.returncode != 0
-
-    # parameter wrong path
-    env['BAKTA_DB'] = 'test/'
-    proc = run(["bin/bakta", 'test/data/NC_002127.1.fna'], env=env)
-    assert proc.returncode != 0
-
     # parameter OK
+    env = os.environ
     env['BAKTA_DB'] = 'test/db'
     proc = run(["bin/bakta", '--output', tmpdir, '--skip-tmrna', '--skip-trna', '--skip-rrna', '--skip-ncrna', '--skip-ncrna-region', '--skip-crispr', '--skip-cds', '--skip-sorf', '--skip-ori', '--skip-gap', 'test/data/NC_002127.1.fna'], env=env)
     assert proc.returncode == 0
@@ -65,11 +78,7 @@ def test_prodigal_tf(tmpdir):
     # OK
     proc = run(["bin/bakta", '--db', 'test/db', '--output', tmpdir, '--prefix', 'test', '--prodigal-tf', 'test/data/prodigal.tf', '--skip-trna', '--skip-rrna', '--skip-ncrna', '--skip-ncrna-region', '--skip-crispr', '--skip-sorf', '--skip-ori', '--skip-gap', 'test/data/NC_002127.1.fna'])
     assert proc.returncode == 0
-    assert Path.exists(Path(tmpdir).joinpath('test.log'))
-    assert Path.exists(Path(tmpdir).joinpath('test.json'))
-    assert Path.exists(Path(tmpdir).joinpath('test.tsv'))
-    assert Path.exists(Path(tmpdir).joinpath('test.gff3'))
-    assert Path.exists(Path(tmpdir).joinpath('test.gbff'))
-    assert Path.exists(Path(tmpdir).joinpath('test.embl'))
-    assert Path.exists(Path(tmpdir).joinpath('test.fna'))
-    assert Path.exists(Path(tmpdir).joinpath('test.faa'))
+
+    tmpdir_path = Path(tmpdir)
+    for file in FILES:
+        assert Path.exists(tmpdir_path.joinpath(file))
--- test/test_bakta.py
+++ test/test_bakta.py
@@ -2,43 +2,43 @@ from pathlib import Path
 from subprocess import run
 
 
+FILES = [
+    'test.log',
+    'test.json',
+    'test.tsv',
+    'test.gff3',
+    'test.gbff',
+    'test.embl',
+    'test.fna',
+    'test.faa'
+]
+
+
 def test_bakta_mock_skipped_features(tmpdir):
     # fast test skipping all feature detections
     proc = run(["bin/bakta", '--db', 'test/db', '--output', tmpdir, '--prefix', 'test', '--skip-tmrna', '--skip-trna', '--skip-rrna', '--skip-ncrna', '--skip-ncrna-region', '--skip-crispr', '--skip-cds', '--skip-sorf', '--skip-ori', '--skip-gap', 'test/data/NC_002127.1.fna'])
     assert proc.returncode == 0
-    assert Path.exists(Path(tmpdir).joinpath('test.log'))
-    assert Path.exists(Path(tmpdir).joinpath('test.json'))
-    assert Path.exists(Path(tmpdir).joinpath('test.tsv'))
-    assert Path.exists(Path(tmpdir).joinpath('test.gff3'))
-    assert Path.exists(Path(tmpdir).joinpath('test.gbff'))
-    assert Path.exists(Path(tmpdir).joinpath('test.embl'))
-    assert Path.exists(Path(tmpdir).joinpath('test.fna'))
-    assert Path.exists(Path(tmpdir).joinpath('test.faa'))
+
+    tmpdir_path = Path(tmpdir)
+    for file in FILES:
+        assert Path.exists(tmpdir_path.joinpath(file))
 
 
 def test_bakta_plasmid(tmpdir):
     # full test on plasmid
     proc = run(["bin/bakta", '--db', 'test/db', '--output', tmpdir, '--prefix', 'test', 'test/data/NC_002127.1.fna'])
     assert proc.returncode == 0
-    assert Path.exists(Path(tmpdir).joinpath('test.log'))
-    assert Path.exists(Path(tmpdir).joinpath('test.json'))
-    assert Path.exists(Path(tmpdir).joinpath('test.tsv'))
-    assert Path.exists(Path(tmpdir).joinpath('test.gff3'))
-    assert Path.exists(Path(tmpdir).joinpath('test.gbff'))
-    assert Path.exists(Path(tmpdir).joinpath('test.embl'))
-    assert Path.exists(Path(tmpdir).joinpath('test.fna'))
-    assert Path.exists(Path(tmpdir).joinpath('test.faa'))
+
+    tmpdir_path = Path(tmpdir)
+    for file in FILES:
+        assert Path.exists(tmpdir_path.joinpath(file))
 
 
 def test_bakta_genome(tmpdir):
     # full test on plasmid
     proc = run(["bin/bakta", '--db', 'test/db', '--output', tmpdir, '--prefix', 'test', 'test/data/GCF_000008865.2.fna.gz'])
     assert proc.returncode == 0
-    assert Path.exists(Path(tmpdir).joinpath('test.log'))
-    assert Path.exists(Path(tmpdir).joinpath('test.json'))
-    assert Path.exists(Path(tmpdir).joinpath('test.tsv'))
-    assert Path.exists(Path(tmpdir).joinpath('test.gff3'))
-    assert Path.exists(Path(tmpdir).joinpath('test.gbff'))
-    assert Path.exists(Path(tmpdir).joinpath('test.embl'))
-    assert Path.exists(Path(tmpdir).joinpath('test.fna'))
-    assert Path.exists(Path(tmpdir).joinpath('test.faa'))
+
+    tmpdir_path = Path(tmpdir)
+    for file in FILES:
+        assert Path.exists(tmpdir_path.joinpath(file))
