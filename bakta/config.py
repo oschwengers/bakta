753e6e80d49ddfe285f6225c8f3347877bafe526
Oliver Schwengers <oliver.schwengers@computational.bio.uni-giessen.de>
2021-11-23 14:11:09 +0100

    fix creation of user defined tmp directory

--- bakta/config.py
+++ bakta/config.py
@@ -119,9 +119,9 @@ def setup(args):
             sys.exit(f'ERROR: dedicated temporary directory ({tmp_path}) does not exist!')
         else:
             log.info('use dedicated temp dir: path=%s', tmp_path)
-            tmp_path = Path(tempfile.mkdtemp(dir=str(tmp_path)))
+            tmp_path = Path(tempfile.mkdtemp(dir=str(tmp_path))).resolve()
     else:
-        tmp_path = Path(tempfile.mkdtemp())
+        tmp_path = Path(tempfile.mkdtemp()).resolve()
     log.info('tmp-path=%s', tmp_path)
 
     try:
