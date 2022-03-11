d83c1b8fa1604a98b36710d14c96514150a8e998
Oliver Schwengers <oliver.schwengers@computational.bio.uni-giessen.de>
2022-03-11 15:31:00 +0100

    fix mol weight in hypo tsv output

--- bakta/io/tsv.py
+++ bakta/io/tsv.py
@@ -42,7 +42,7 @@ def write_hypothetical_tsv(hypotheticals: Sequence[dict], tsv_path: Path):
         for hypo in hypotheticals:
             pfams = [f"{pfam['id']}|{pfam['name']}" for pfam in hypo.get('pfams', [])]
             seq_stats = hypo['seq_stats']
-            mol_weight = f"{(seq_stats['molecular_weight']/100):.1f}" if seq_stats['molecular_weight'] else 'NA'
+            mol_weight = f"{(seq_stats['molecular_weight']/1000):.1f}" if seq_stats['molecular_weight'] else 'NA'
             iso_point = f"{seq_stats['isoelectric_point']:.1f}" if seq_stats['isoelectric_point'] else 'NA'
             fh.write(f"{hypo['contig']}\t{hypo['start']}\t{hypo['stop']}\t{hypo['strand']}\t{hypo.get('locus', '')}\t{mol_weight}\t{iso_point}\t{', '.join(sorted(pfams))}\t{', '.join(sorted(hypo.get('db_xrefs', [])))}\n")
     return
