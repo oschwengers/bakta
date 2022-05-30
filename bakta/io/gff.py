85c9c4215fed5ffa7e7e846270db1ecec41c9a37
Oliver Schwengers <oliver.schwengers@computational.bio.uni-giessen.de>
2022-05-30 10:51:00 +0200

    add compliant ncRNA region inference GFF3 tags

--- bakta/io/gff.py
+++ bakta/io/gff.py
@@ -167,6 +167,10 @@ def write_gff3(genome: dict, features_by_contig: Dict[str, dict], gff3_path: Pat
                         'Dbxref': feat['db_xrefs']
                     }
                     if(cfg.compliant):
+                        for dbxref in feat['db_xrefs']:
+                            if(dbxref.split(':')[0] == bc.DB_XREF_RFAM):
+                                rfam_id = dbxref.split(':')[1]
+                                annotations['inference'] = f'profile:Rfam:{rfam_id}'
                         annotations['Dbxref'], annotations['Note'] = insdc.revise_dbxref_insdc(feat['db_xrefs'])  # remove INSDC invalid DbXrefs
                         annotations[bc.INSDC_FEATURE_REGULATORY_CLASS] = insdc.select_regulatory_class(feat)
                     annotations = encode_annotations(annotations)
