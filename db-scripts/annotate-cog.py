46e645a9d20084ad9e784467ebea971c87223837
Oliver Schwengers <oliver.schwengers@computational.bio.uni-giessen.de>
2022-06-09 17:02:07 +0200

    make COG annotation mandatory

--- db-scripts/annotate-cog.py
+++ db-scripts/annotate-cog.py
@@ -89,10 +89,10 @@ with alignments_path.open() as fh, sqlite3.connect(str(db_path), isolation_level
                 psc_updated += 1
                 rec_psc = conn.execute('SELECT * FROM psc WHERE uniref90_id=?', (uniref90_id,)).fetchone()
                 if(rec_psc is not None):
-                    if(rec_psc['product'] is None and cog['product'] is not None):
+                    if(cog['product'] is not None):
                         conn.execute('UPDATE psc SET product=? WHERE uniref90_id=?', (cog['product'], uniref90_id))
                         log.info('UPDATE psc SET product=%s WHERE uniref90_id=%s', cog['product'], uniref90_id)
-                    if(rec_psc['gene'] is None and cog['gene'] is not None):
+                    if(cog['gene'] is not None):
                         conn.execute('UPDATE psc SET gene=? WHERE uniref90_id=?', (cog['gene'], uniref90_id))
                         log.info('UPDATE psc SET gene=%s WHERE uniref90_id=%s', cog['gene'], uniref90_id)
         psc_processed += 1
