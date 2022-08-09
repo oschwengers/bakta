2c7dd81dde66212cf76127120bf6c13a7a7b1994
Jannik Lübke <Jannik.Luebke@bio.uni-giessen.de>
2022-08-09 14:49:39 +0200

    add adjusting tick labels, Bug fixes

--- bakta/plot.py
+++ bakta/plot.py
@@ -3,7 +3,7 @@ from subprocess import run
 from Bio import SeqUtils
 
 import bakta.config as cfg
-
+import bakta.constants as bc
 
 def write_plot(features,
                contigs,
@@ -19,7 +19,7 @@ def write_plot(features,
     # 1Plot/sequence on/off
     ##########################
     if all_plots == True:
-        outdir = outdir.joinpath('Plots')
+        outdir = outdir.joinpath(f'{cfg.prefix}_Plots')    #Ordner Ja Nein?
         Path(outdir).mkdir(parents=True, exist_ok=True)
         for c in contigs:
             if c['complete'] == True:
@@ -29,7 +29,6 @@ def write_plot(features,
                     if f['contig'] == c['id']:
                         contig_features.append(f)
                 plot_count += 1
-
                 write_plot(contig_features,
                             contig_contigs,
                             outdir,
@@ -43,18 +42,27 @@ def write_plot(features,
     #######################
     # Define Variables
     #######################
-    genome_lengths = 0
-    window_sizes = []
-    step_sizes = []
+    added_sequens_lenght = 0
+    added_sequences = ""
     for c in contigs:
-        genome_lengths += int(c['length'])
-        window_size = c['length']/100 if c['length'] < 2000 else c['length'] / 1000
-        window_size = 10 if window_size < 10 else window_size
-        window_sizes.append(window_size)
-        step_size = window_size* 0.2 if window_size >= 5 else 1
-        step_sizes.append(step_size)
-
+        added_sequens_lenght += int(c['length'])
+        added_sequences += c['sequence']
+        print(added_sequens_lenght)
+    window_size = added_sequens_lenght/100 if added_sequens_lenght < 2000 else added_sequens_lenght/1000
+    window_size = 3 if window_size < 3 else window_size
+    step_size = window_size* 0.2 if window_size >= 5 else 1
     track_radius = 1.0
+    gc_radius = 0.2
+
+    label_prefix = 'b'
+    multiplier = 1
+    if added_sequens_lenght > 1000:
+        label_prefix = 'kb'
+        multiplier = 0.001
+    if added_sequens_lenght > 1000000:
+        label_prefix = 'mb'
+        multiplier = 0.000001
+
 
     ###########################
     # Config Paths
@@ -74,7 +82,7 @@ def write_plot(features,
     gc_skew_file = f'{config_dir}/gc_skew.txt'
 
     #####################
-    # write featurefiles
+    # write feature files
     #####################
     f_cds_contents = ""
     r_cds_contents = ""
@@ -84,9 +92,9 @@ def write_plot(features,
         contig, start, stop = f['contig'], f['start'], f['stop']
 
         if f['type'] == 'cds':
-            if f['strand'] == '+':
+            if f['strand'] == bc.STRAND_FORWARD:
                 f_cds_contents += f"{contig} {start} {stop} {f['strand']} color={color}\n"
-            if f['strand'] == '-':
+            if f['strand'] == bc.STRAND_REVERSE:
                 r_cds_contents += f"{contig} {start} {stop} {f['strand']} color={color}\n"
             else:
                 continue
@@ -107,10 +115,9 @@ def write_plot(features,
     gc_skew_text = ""
     max_gc_content = 0
     max_gc_skew = 0
-    x = zip(contigs, step_sizes, window_sizes)
-    for contig, step_size, window_size in x:
+    gc_mean = SeqUtils.GC(added_sequences)
+    for contig in contigs:
         seq = contig['sequence']
-        gc_mean = SeqUtils.GC(seq)
         step_size = int(step_size)
         window_size = int(window_size)
         for w in range(0, len(seq), step_size):
@@ -129,7 +136,7 @@ def write_plot(features,
             gc_value = gc_mean - SeqUtils.GC(subseq)
             max_gc_content = abs(gc_value) if max_gc_content < abs(gc_value) else max_gc_content
             content_color = p_gc_content_color if gc_value > 0 else n_gc_content_color
-            gc_content_text += f"{contig['id']} {w} {w + step_size} {gc_value} fill_color={content_color}\n"
+            gc_content_text += f"{contig['id']} {w} {w} {gc_value} fill_color={content_color}\n"
             G = float(subseq.count('G'))
             C = float(subseq.count('C'))
             try:
@@ -138,7 +145,7 @@ def write_plot(features,
                 gc_skew = 0.0
             max_gc_skew = abs(gc_skew) if max_gc_skew < abs(gc_skew) else max_gc_skew
             skew_color = p_gc_skew_color if gc_skew > 0 else n_gc_skew_color
-            gc_skew_text += f"{contig['id']} {w} {w + step_size} {gc_skew} fill_color={skew_color}\n"
+            gc_skew_text += f"{contig['id']} {w} {w} {gc_skew} fill_color={skew_color}\n"
 
     with open(gc_content_file, 'w') as f:
         f.write(gc_content_text)
@@ -152,7 +159,7 @@ def write_plot(features,
     #write main config
     main_config_text = f'''
     karyotype                   = {karyotype_txt}
-    chromosomes_units           = {10 ** (len(str(genome_lengths)) - 1)}
+    chromosomes_units           = {added_sequens_lenght}
     chromosomes_display_default = yes
     <plots>
     <<include {tracks_conf}>>
@@ -172,10 +179,10 @@ def write_plot(features,
 
     #write_karyotype_file
     karyotype_text = ""
-    count = 0
-    colors = [ 'dgrey', 'grey', 'red', 'black']
+
+    colors = [ 'dgrey', 'grey', 'hgrey','red', 'black']
     for i, c in enumerate(contigs):
-        karyotype_text += f"chr - {c['id']} {i + 1} {count} {len(c['sequence'])} {colors[i]}\n"
+        karyotype_text += f"chr - {c['id']} {i + 1} 0 {c['length']} {colors[i]}\n"
 
     with open(karyotype_txt, 'w') as f:
         f.write(karyotype_text)
@@ -184,47 +191,45 @@ def write_plot(features,
     ideogram_text = '''
     <ideogram>
     <spacing>
-    default = 0.005r
+    default = 0.01r
     </spacing>
-    radius           = 0.80r
-    thickness        = 15p
+    radius           = 0.85r
+    thickness        = 5p
     fill             = yes
-    stroke_color     = dgrey
-    show_bands       = yes
-    fill_bands       = yes
-    stroke_thickness = 2p
-    show_label       = no
-    label_font       = default
-    label_radius     = 1r + 75p
-    label_size       = 30
-    label_parallel   = yes
     </ideogram>
     '''
     with open(ideogram_conf, 'w') as f:
         f.write(ideogram_text)
 
     #write_ticks_config:
-    ticks_text = '''
+    ticks_text = f'''
     show_ticks       = yes
     show_tick_labels = yes
     <ticks>
     radius      = 1r
     color       = black
     thickness   = 2p
-    multiplier  = 1e-6
+    multiplier  = {multiplier}
     orientation = out
-    format      = %.1f Mb
+    format      = %.01f{label_prefix}
     <tick>
-    spacing      = 0.5u
+    spacing      = 0.15u
     show_label   = yes
     label_size   = 35
     label_offset = 10p
     size         = 25p
+    thickness    = 4p
     </tick>
     <tick>
-    spacing      = 0.5u
-    show_label   = no
+    spacing      = 0.015u
+    show_label   = yes
+    label_size   = 15
     size         = 15p
+    thickness    = 3p
+    </tick>
+    <tick>
+    spacing      = 0.0015u
+    size         = 5p
     </tick>
     </ticks>
     '''
@@ -245,7 +250,7 @@ def write_plot(features,
         orientation      = out
         layers           = 1
         margin           = 0.01u
-        thickness        = 70
+        thickness        = 100
         padding          = 1
         stroke_color     = black
         stroke_thickness = 0
@@ -264,7 +269,7 @@ def write_plot(features,
         type = histogram
         file = {gc_file}
         r1 = {track_radius}r
-        r0 = {track_radius-0.15}r
+        r0 = {track_radius-gc_radius}r
         min = {minimum}
         max = {maximum}
         thickness = 0
@@ -272,10 +277,9 @@ def write_plot(features,
         </plot>    
         '''
 
-        track_radius -= 0.15
+        track_radius -= gc_radius
     with open(tracks_conf, 'w') as f:
         f.write(tracks_text)
-
     #############
     # run Circos
     #############
