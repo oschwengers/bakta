830e1abdbd45a63f280d41c877000b06f6a78488
Oliver Schwengers <oliver.schwengers@computational.bio.uni-giessen.de>
2022-09-09 16:10:01 +0200

    introduce debug option #137 (#141)

--- bakta/utils.py
+++ bakta/utils.py
@@ -105,6 +105,7 @@ def parse_arguments():
     arg_group_general = parser.add_argument_group('General')
     arg_group_general.add_argument('--help', '-h', action='help', help='Show this help message and exit')
     arg_group_general.add_argument('--verbose', '-v', action='store_true', help='Print verbose information')
+    arg_group_general.add_argument('--debug', action='store_true', help='Run Bakta in debug mode. Temp data will not be removed.')
     arg_group_general.add_argument('--threads', '-t', action='store', type=int, default=0, help='Number of threads to use (default = number of available CPUs)')
     arg_group_general.add_argument('--tmp-dir', action='store', default=None, dest='tmp_dir', help='Location for temporary files (default = system dependent auto detection)')
     arg_group_general.add_argument('--version', action='version', version=f'%(prog)s {bakta.__version__}')
