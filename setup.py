734eeaffd6812bdb79bb20bdeca65d22c403caa7
Oliver Schwengers <oliver.schwengers@computational.bio.uni-giessen.de>
2022-10-18 12:19:37 +0200

    add PyYAML dependency #163

--- setup.py
+++ setup.py
@@ -29,7 +29,8 @@ setup(
         'biopython >= 1.78',
         'xopen >= 1.1.0',
         'requests >= 2.25.1',
-        'alive-progress == 1.6.2'
+        'alive-progress == 1.6.2',
+        'PyYAML >= 6.0'
     ],
     entry_points={
         'console_scripts': [
