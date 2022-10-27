b51ad75334347452babe656c54b9d75ed223a34b
Oliver Schwengers <oliver.schwengers@computational.bio.uni-giessen.de>
2022-10-27 13:35:43 +0200

    amend CI tests #163

--- test/test_user_proteins.py
+++ test/test_user_proteins.py
@@ -150,7 +150,7 @@ def test_user_proteins(parameters, tmpdir):
     proc = run(
         [
             'bin/bakta', '--db', 'test/db', '--output', tmpdir, '--prefix', 'test', '--proteins', parameters,
-            '--skip-tmrna', '--skip-trna', '--skip-rrna', '--skip-ncrna', '--skip-ncrna-region', '--skip-crispr', '--skip-sorf', '--skip-ori', '--skip-gap', 
+            '--skip-tmrna', '--skip-trna', '--skip-rrna', '--skip-ncrna', '--skip-ncrna-region', '--skip-crispr', '--skip-sorf', '--skip-ori', '--skip-gap', '--skip-plot',
             'test/data/NC_002127.1.fna'
         ]
     )
