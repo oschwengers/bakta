9632b90217b936ffe0c61d6868a0727e3a05677d
Oliver Schwengers <oliver.schwengers@computational.bio.uni-giessen.de>
2022-11-24 09:46:48 +0100

    add anticodon to tRNA products

--- bakta/features/t_rna.py
+++ bakta/features/t_rna.py
@@ -91,7 +91,7 @@ def predict_t_rnas(genome: dict, contigs_path: Path):
             trna['product'] = 'tRNA-Xxx'
             if(trna_type != 'Undet' and trna_type != 'Sup'):
                 trna['gene'] = f'{trna_type}_trna'
-                trna['product'] = f'tRNA-{trna_type}'
+                trna['product'] = f'tRNA-{trna_type}({anti_codon.lower()})'
                 trna['amino_acid'] = trna_type
                 trna['anti_codon'] = anti_codon.lower()
 
