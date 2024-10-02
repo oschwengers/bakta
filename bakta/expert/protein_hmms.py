0e802d6ea857da4ad70969ee331f1fd47551b74a
Oliver Schwengers <oliver.schwengers@computational.bio.uni-giessen.de>
2024-10-02 15:59:42 +0200

    finish user-hmm feature

--- bakta/expert/protein_hmms.py
+++ bakta/expert/protein_hmms.py
@@ -12,32 +12,21 @@ import bakta.features.orf as orf
 log = logging.getLogger('EXPERT_AA_HMM')
 
 
-def search(cdss: Sequence[dict], user_hmms):
+def search(cdss: Sequence[dict], user_hmms_path):
     """Detect Pfam-A entries"""
     cds_found = set()
     orf_by_aa_digest = orf.get_orf_dictionary(cdss)
     alphabet: pyhmmer.easel.Alphabet = pyhmmer.easel.Alphabet.amino()
     proteins: list[pyhmmer.easel.DigitalSequence] = [ pyhmmer.easel.TextSequence(sequence=cds['aa'], name=bytes(orf.get_orf_key(cds), 'UTF-8')).digitize(alphabet) for cds in cdss ]
-    with pyhmmer.plan7.HMMFile(user_hmms) as hmm:
-        for hmm_query_hits in pyhmmer.hmmsearch(hmm, proteins, bit_cutoffs='trusted', cpus=cfg.threads):
-            print(f'TopHits: {hmm_query_hits}')
-            hmm_name = hmm_query_hits.query_name.decode()
-            print(f'query_name: {hmm_name}')
+    with pyhmmer.plan7.HMMFile(user_hmms_path) as hmms_fh:
+        hmms = list(hmms_fh)
+        hmms_id_desc = { hmm.accession.decode(): hmm.description.decode() for hmm in hmms }
+        for hmm_query_hits in pyhmmer.hmmsearch(hmms, proteins, bit_cutoffs='trusted', cpus=cfg.threads):
             hmm_id = hmm_query_hits.query_accession.decode()
-            print(f'query_accession: {hmm_id}')
             hmm_length = hmm_query_hits.query_length
-            print(f'query_length {hmm_length}')
-            # print(f'description: {hmm_query_hits.description.decode()}')
-            
+            hmm_description = hmms_id_desc.get(hmm_id, '')
+            hmms_description_fields = hmm_description.split('~~~')
             for hmm_query_hit in hmm_query_hits.reported:
-                print(f'Hit: {hmm_query_hit}')
-                # print(f'\taccession: {hmm_query_hit.accession.decode()}')
-                # print(f'\tdescription: {hmm_query_hit.description.decode()}')
-                print(f'\tname: {hmm_query_hit.name.decode()}')
-                print(f'\tevalue: {hmm_query_hit.evalue}')
-                print(f'\tlength: {hmm_query_hit.length}')
-                print(f'\tscore: {hmm_query_hit.score}')
-
                 aa_identifier = hmm_query_hit.name.decode()
                 cds = orf_by_aa_digest[aa_identifier]
                 if hmm_query_hit.evalue > bc.MIN_HMM_EVALUE:
@@ -47,47 +36,38 @@ def search(cdss: Sequence[dict], user_hmms):
                     )
                 else:
                     hit_domain_lengths_sum = sum([len(dom.alignment.hmm_sequence) for dom in hmm_query_hit.domains.included])
-                    print(f'hit domain length sum: {hit_domain_lengths_sum}')
-                    hmm_cov = hit_domain_lengths_sum / hmm_length
-                    print(f'domain cov: {hmm_cov}')
-                    aa_cov = hit_domain_lengths_sum / hmm_length
-                    aa_cov_best_domain = (hmm_query_hit.best_domain.alignment.target_to - hmm_query_hit.best_domain.alignment.target_from + 1) / len(cds['aa'])
-                    assert aa_cov == aa_cov_best_domain, f'ERROR: aa_cov ({aa_cov}) != aa_cov_best_domain ({aa_cov_best_domain})'
-                    print(f'aa cov: {aa_cov}')
-
                     hit = {
                         'type': 'user_hmms',
                         'source': 'UserHMMs',
                         'rank': 100,
                         'id': hmm_id,
                         'length': hit_domain_lengths_sum,
-                        'aa_cov': aa_cov,
-                        'hmm_cov': hmm_cov,
+                        'aa_cov': hit_domain_lengths_sum / len(cds['aa']),
+                        'hmm_cov': hit_domain_lengths_sum / hmm_length,
                         'evalue': hmm_query_hit.evalue,
                         'score': hmm_query_hit.score,
                         'start': hmm_query_hit.best_domain.alignment.target_from,
                         'stop': hmm_query_hit.best_domain.alignment.target_to,
                         'db_xrefs': [f'UserHMM:{hmm_id}'],
                         'gene': None,
-                        'product': hmm_name  # None
+                        'product': None
                     }
-
-                    # ToDo: implement user-provided gene, product, db_xrefs via HMM's description field provided as <gene>~~~<product>~~~<db_xrefs>, waiting for https://github.com/althonos/pyhmmer/issues/76
-                    # hmm_description = hmm_query_hits.description.decode()
-                    # cols = hmm_description.split('~~~')
-                    # if(len(cols) == 1):
-                    #     hit['product'] = hmm_description
-                    # else:
-                    #     (gene, product, dbxrefs) = cols
-                    #     hit['gene'] = gene
-                    #     hit['product'] = product
-                    #     hit['db_xrefs'].extend(set(dbxrefs.split(',')))
+                    # read user-provided gene, product, db_xrefs via HMM's description field provided as <gene>~~~<product>~~~<db_xrefs>
+                    if(len(hmms_description_fields) == 1):
+                        hit['product'] = hmm_description if hmm_description != '' else None
+                    elif(len(hmms_description_fields) == 3):
+                        (gene, product, dbxrefs) = hmms_description_fields
+                        hit['gene'] = gene
+                        hit['product'] = product
+                        hit['db_xrefs'].extend(set(dbxrefs.split(',')))
+                    else:
+                        log.warning('wrong HMM description format: id=%s, desc=%s', hmm_id, hmm_description)
 
                     cds.setdefault('expert', [])
                     cds['expert'].append(hit)
                     log.debug(
-                        'hit: source=UserHMMs, rank=99, contig=%s, start=%i, stop=%i, strand=%s, query-cov=%0.3f, model-cov=%0.3f, gene=%s, product=%s, evalue=%1.1e, bitscore=%f',
-                        cds['contig'], cds['start'], cds['stop'], cds['strand'], hit['aa_cov'], hit['hmm_cov'], hit['gene'], hit['product'], hit['evalue'], hit['score']
+                        'hit: source=UserHMMs, rank=99, contig=%s, start=%i, stop=%i, strand=%s, query-cov=%0.3f, model-cov=%0.3f, hmm-id=%s, gene=%s, product=%s, evalue=%1.1e, bitscore=%f',
+                        cds['contig'], cds['start'], cds['stop'], cds['strand'], hit['aa_cov'], hit['hmm_cov'], hmm_id, hit['gene'], hit['product'], hit['evalue'], hit['score']
                     )
                     cds_found.add(aa_identifier)
 
