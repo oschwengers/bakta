e0b96923099926eeec9add330c1c499302bb5941
Oliver Schwengers <oliver.schwengers@computational.bio.uni-giessen.de>
2026-01-27 20:49:30 +0100

    rewrite and optimize SQLite code

--- bakta/ips.py
+++ bakta/ips.py
@@ -1,11 +1,11 @@
 import logging
-import sqlite3
 
 from concurrent.futures import ThreadPoolExecutor
 from typing import Sequence, Tuple
 
 import bakta.config as cfg
 import bakta.constants as bc
+import bakta.utils as bu
 
 
 ############################################################################
@@ -28,20 +28,17 @@ def lookup(features: Sequence[dict]) -> Tuple[Sequence[dict], Sequence[dict]]:
         features_found = []
         features_not_found = []
         rec_futures = []
-        with sqlite3.connect(f"file:{cfg.db_path.joinpath('bakta.db')}?mode=ro&nolock=1&cache=shared", uri=True, check_same_thread=False) as conn:
-            conn.execute('PRAGMA omit_readlock;')
-            conn.row_factory = sqlite3.Row
-            with ThreadPoolExecutor(max_workers=max(10, cfg.threads)) as tpe:  # use min 10 threads for IO bound non-CPU lookups
-                for feature in features:
-                    if('truncated' not in feature):  # skip truncated CDS
-                        uniref100_id = feature.get('ups', {}).get('uniref100_id', None)
-                        if(uniref100_id):
-                            future = tpe.submit(fetch_db_ips_result, conn, feature)
-                            rec_futures.append((feature, future))
-                        else:
-                            features_not_found.append(feature)
+        with ThreadPoolExecutor(max_workers=max(10, cfg.threads)) as tpe:  # use min 10 threads for IO bound non-CPU lookups
+            for feature in features:
+                if('truncated' not in feature):  # skip truncated CDS
+                    uniref100_id = feature.get('ups', {}).get('uniref100_id', None)
+                    if(uniref100_id):
+                        future = tpe.submit(fetch_db_ips_result, feature)
+                        rec_futures.append((feature, future))
                     else:
                         features_not_found.append(feature)
+                else:
+                    features_not_found.append(feature)
 
         for (feature, future) in rec_futures:
             rec = future.result()
@@ -63,12 +60,13 @@ def lookup(features: Sequence[dict]) -> Tuple[Sequence[dict], Sequence[dict]]:
         raise Exception('SQL error!', ex)
 
 
-def fetch_db_ips_result(conn: sqlite3.Connection, feature: dict):
-    c = conn.cursor()
-    c.execute('select * from ips where uniref100_id=?', (feature['ups']['uniref100_id'][10:],))
-    rec = c.fetchone()
-    c.close()
-    return rec
+def fetch_db_ips_result(feature: dict):
+    with bu.get_db_connection() as conn:
+        c = conn.cursor()
+        c.execute('select * from ips where uniref100_id=?', (feature['ups']['uniref100_id'][10:],))
+        rec = c.fetchone()
+        c.close()
+        return rec
 
 
 def parse_annotation(rec) -> dict:
